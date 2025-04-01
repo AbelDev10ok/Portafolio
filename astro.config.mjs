0c6277c68c58e5721cb1e8fda01a0a7c8ba33d5f
AbelDev10ok <abel.alarcon.dev@gmail.com>
2025-04-01 19:07:27 -0300

    add conf vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   vite: {
     ssr: {
       noExternal: ['@fontsource-variable/onest'], // Replace with your font package name
     },
   },
+
+  output: 'server',
+  // @ts-ignore
+  adapter: vercel(),
 });
